64f6b266ed4d488616da83583367b4d8e28be949
kadmiv <kachulyak.ivan@gmail.com>
2019-07-02 11:11:49 +0300

    Some change

--- app/src/main/java/com/example/androidrestserver/MainActivity.kt
+++ app/src/main/java/com/example/androidrestserver/MainActivity.kt
@@ -3,24 +3,24 @@ package com.example.androidrestserver
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.example.androidrestserver.server.MainServer
 import com.example.androidrestserver.utils.Utils
-import ru.skornei.restserver.RestServerManager
-import android.system.Os.listen
 import com.google.gson.Gson
 import com.koushikdutta.async.http.WebSocket
 import com.koushikdutta.async.http.server.AsyncHttpServer
 import com.koushikdutta.async.http.server.AsyncHttpServerRequest
+import kotlinx.android.synthetic.main.activity_main.*
 import com.koushikdutta.async.http.server.AsyncHttpServerResponse
 import com.koushikdutta.async.http.server.HttpServerRequestCallback
-import org.json.JSONObject
 
 
 const val ld = "12"
 
 class MainActivity : AppCompatActivity() {
 
-    var mainServer = MainServer()
+    var mainServer = AsyncHttpServer()
+    val PORT = 8080
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,8 +28,8 @@ class MainActivity : AppCompatActivity() {
 
         val locIPv4 = Utils.getIPAddress(true); // IPv4
         val locIPv6 = Utils.getIPAddress(false); // IPv6
-
-        Log.d(ld, "locIPv4: $locIPv4 | locIPv6: $locIPv6")
+        val msg = "locIPv4: $locIPv4 | locIPv6: $locIPv6"
+        Log.d(ld, msg)
 
 //        RestServerManager.initialize(application)
 //        mainServer.start()
@@ -38,17 +38,32 @@ class MainActivity : AppCompatActivity() {
 
         val _sockets = ArrayList<WebSocket>()
 
-        server.get("/test") { request, response -> response.send("Hello!!!") }
+        server.get(
+            "/"
+        ) { request, response -> response.send("Hello!!!") }
+
+        server.get("/test") { request, response ->
+            makeToast(request)
+            response.send("Hello!!!")
+        }
 
         server.post("/test") { request, response ->
-            val requestString = request.body.get()
-            Log.d(ld, "requestString : $requestString")
-//            Log.d(ld, Gson().toJson(requestString))
+            makeToast(request)
             val jsonResponse = Gson().toJson(Hello())
             response.send(jsonResponse)
         }
-// listen on port 5000
-        server.listen(5000)
+
+        // listen on port
+        server.listen(PORT)
+        ip_text.text = msg
+
+    }
+
+    private fun makeToast(request: AsyncHttpServerRequest) {
+        val requestString = request.body.get()
+        val msg = "requestString : $requestString"
+        Log.d(ld, msg)
+        Toast.makeText(applicationContext, msg, Toast.LENGTH_LONG).show()
     }
 
     override fun onDestroy() {
